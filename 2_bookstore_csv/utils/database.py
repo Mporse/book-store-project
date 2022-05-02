28f82e898127bd45ac02eddb5f9b5208fecf12f7
Mads Pedersen <madsporsepedersen@gmail.com>
2022-05-02 17:14:47 +0200

    Add code for functions from program 1_bookstore to utils

--- 2_bookstore_csv/utils/database.py
+++ 2_bookstore_csv/utils/database.py
@@ -3,3 +3,4 @@ This is a movie database/storage program.
 The program uses built-in Python data structures when working with user inputs,
 but contains the ability to save and load the database to/from CSV files as well.
 """
+
